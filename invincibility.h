672e0036e7009d72f8fca413227dbbfd16464289
wujunhao <u3638850@connect.hku.hk>
2025-04-30 23:13:42 +0800

    Update invincibility.h

--- invincibility.h
+++ invincibility.h
@@ -3,16 +3,25 @@
 
 class Invincibility {
 private:
-    int remainingMoves;  // Number of safe moves remaining
+    int remainingMoves;  // Number of remaining protected moves
     bool isActive;       // Whether invincibility is currently active
 
 public:
     Invincibility();
     
+    // Activate invincibility with specified number of moves
     void activate(int moves);
-    bool useMove();      // Returns true if invincibility was used
+    
+    // Use one invincibility move
+    void useMove();
+    
+    // Check if invincibility is currently active
     bool isInvincible() const;
+    
+    // Get remaining protected moves
     int getRemainingMoves() const;
+    
+    // Deactivate invincibility
     void deactivate();
 };
 
