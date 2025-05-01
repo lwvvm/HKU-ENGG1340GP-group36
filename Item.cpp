716884ff03335e1d73454d697be06d090a0ffc7a
LinZhongxi <u3629153@connect.hku.hk>
2025-05-01 15:45:55 +0800

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,15 +1,24 @@
 #include "Item.h"
-#include <iostream>
 #include <sstream>
+#include <iostream>
 
 using namespace std;
 
-void performAutoSweep(
-    vector<vector<bool>> &revealed,
-    const vector<vector<bool>> &mineGrid,
+Item::Item() : 
+    remainingReveals(0),
+    invincibilityActive(false),
+    shieldActive(false),
+    shieldActivationTime(0),
+    shieldDurationSeconds(0) 
+{}
+
+// AutoSweep implementation
+void Item::performAutoSweep(
+    vector<vector<bool>>& revealed,
+    const vector<vector<bool>>& mineGrid,
     int rows,
-    int cols
-) {
+    int cols) 
+{
     int r = -1, c = -1;
     while (true) {
         cout << "Enter the center of the 3x3 area (e.g., '2 3'): ";
@@ -36,7 +45,6 @@ void performAutoSweep(
         break;
     }
 
-    // Reveal mines in the 3x3 range
     for (int dr = -1; dr <= 1; ++dr) {
         for (int dc = -1; dc <= 1; ++dc) {
             int nr = r + dr;
@@ -48,4 +56,61 @@ void performAutoSweep(
     }
 
     cout << "\033[1;33mMines in the 3x3 area have been revealed!\033[0m\n";
+}
+
+// Invincibility implementations
+void Item::activateInvincibility(int reveals) {
+    remainingReveals = reveals;
+    invincibilityActive = true;
+}
+
+void Item::countInvincibilityReveal() {
+    if (!invincibilityActive) return;
+    
+    remainingReveals--;
+    if (remainingReveals <= 0) {
+        deactivateInvincibility();
+    }
+}
+
+bool Item::isInvincible() const {
+    return invincibilityActive && remainingReveals > 0;
+}
+
+int Item::getRemainingInvincibilityReveals() const {
+    return remainingReveals;
+}
+
+void Item::deactivateInvincibility() {
+    invincibilityActive = false;
+    remainingReveals = 0;
+}
+
+// Shield implementations
+void Item::activateShield(int seconds) {
+    shieldActive = true;
+    shieldActivationTime = time(nullptr);
+    shieldDurationSeconds = seconds;
+}
+
+void Item::deactivateShield() {
+    shieldActive = false;
+    cout << "\033[1;34mShield has expired.\033[0m\n";
+}
+
+bool Item::isShieldActive() const {
+    return shieldActive && 
+           (difftime(time(nullptr), shieldActivationTime) < shieldDurationSeconds);
+}
+
+int Item::getShieldRemainingTime() const {
+    if (!shieldActive) return 0;
+    int remaining = shieldDurationSeconds - difftime(time(nullptr), shieldActivationTime);
+    return remaining > 0 ? remaining : 0;
+}
+
+// Combined protection
+bool Item::isProtected() const {
+    return (invincibilityActive && remainingReveals > 0) || 
+           (shieldActive && (difftime(time(nullptr), shieldActivationTime) < shieldDurationSeconds));
 }
